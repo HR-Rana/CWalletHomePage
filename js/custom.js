096f5cb89d88dc2870ce9b8e251c80a710de13a8
Rana <76555387+HR-Rana@users.noreply.github.com>
2023-09-06 19:48:16 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+
+
+
+   $("#one").click(function(){
+        $("#one p").toggle(100);
+    });
+   $("#two").click(function(){
+        $("#two p").toggle(100);
+    });
+   $("#three").click(function(){
+        $("#three p").toggle(100);
+    });
+   $("#four").click(function(){
+        $("#four p").toggle(100);
+    });
